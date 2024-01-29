4e564993a3708162847ffbff9e76125d9e4d623c
RiadMefti <riadmefti@hotmail.com>
2024-01-29 16:34:35 -0500

    construct the command modal

--- src/components/command-palette/CommandPalette.tsx
+++ src/components/command-palette/CommandPalette.tsx
@@ -1,24 +1,51 @@
 import { FC } from "react";
 import useCommandPaletteStore from "../../stores/CommandPaletteStore";
 import { Modal } from "@mantine/core";
+import { Kbd } from "@mantine/core";
+interface Commands {
+  name: string;
+  shortcut: string;
+}
 
-interface CommandPaletteProps {}
+const commands: Commands[] = [
+  { name: "Create new file", shortcut: "mod+shift+C" },
+  { name: "Create new directory", shortcut: "mod+O" },
+  { name: "Open file", shortcut: "mod+shift+O" },
+  { name: "Save file", shortcut: "mod+s" },
+  { name: "Change tab", shortcut: "shift+tab" },
+  { name: "Open command palette", shortcut: "mod+P" },
+];
 
-const CommandPalette: FC<CommandPaletteProps> = ({}) => {
+const CommandPalette: FC = () => {
   const { openCommandPalette, setOpenCommandPalette } =
     useCommandPaletteStore();
 
   const handleClose = () => {
     setOpenCommandPalette(false);
   };
+  const processShortcut = (shortcut: string) => {
+    const keys = shortcut.split("+");
 
+    return keys.map((key, index) => (
+      <Kbd key={index}>{key}</Kbd> // Use the Kbd component directly
+    ));
+  };
   return (
     <Modal
       opened={openCommandPalette}
       onClose={handleClose}
-      title="Authentication"
+      title="Command Palette"
+      size="xl"
+      
     >
-      {/* Modal content */}
+      <div style={{height:'70vh'}}>
+        {commands.map((command) => (
+          <div>
+            <span>{command.name}</span>
+            <span>{processShortcut(command.shortcut)}</span>
+          </div>
+        ))}
+      </div>
     </Modal>
   );
 };
